735a828d7200129f471e17e51a9ec16fa24c5631
bimarshak7 <khanalbimarsha7@gmail.com>
2024-01-19 22:25:13 +0545

    Endpoint to classify garbage

--- garbage_cls/app.py
+++ garbage_cls/app.py
@@ -1,58 +1,39 @@
-from ultralytics import YOLO
+import torch
+from torchvision.io import read_image
+from torchvision import transforms
 from flask import Flask, request
 from PIL import Image
-import uuid
-import cv2
-import random
 
-app = Flask(__name__)
-
-
-print("Loading model")
-model = YOLO("./best.pt")
+model = torch.jit.load('garbage_jit.pt')
 print("Model loaded")
 
-colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(24)]
+# Define class names, image size and transform function
+cs = ['Organic', 'Renewable']
+IMG_SIZE = (384,384)
+transform = transforms.Compose([
+    transforms.Resize(IMG_SIZE,antialias=True),
+    transforms.ToTensor(),
+])
 
+app = Flask(__name__)
 
 @app.route("/api/predict", methods=["POST"])
 def predictImage():
     image = request.files["img"]
     if image is None:
         return {"message": "No image received"}
+    im = Image.open(image)
+    # The image can be converted to tensor using
+    im = transform(im)
+    im = im.to("cpu")
+    pred = model(im.unsqueeze(0))
+    pred_cls = cs[pred.argmax(1)]
 
-    print("[*]Detecting Image")
-    result = model([Image.open(image)], conf=0.6)[0]
-    print("[.]Detection complete")
-    print("Result:",result.boxes)
-    data = result.boxes.data.cpu().tolist()
-    h, w = result.orig_shape
-
-    names = result.names
-
-    r = []
-    for row in data:
-        box = [row[0] / w, row[1] / h, row[2] / w, row[3] / h]
-        conf = row[4]
-        classId = int(row[5])
-        name = names[classId]
-        r.append(
-            {
-                "box": box,
-                "confidence": conf,
-                "classId": classId,
-                "name": name,
-                "color": "#%02x%02x%02x" % tuple(colors[classId]),
-            }
-        )
-    # print("Frame: ",r)
-    return {"frame": r}
-
+    return {"class": pred_cls}
 
 @app.route("/")
 def home():
     return {"message": "Moye moye"}
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
